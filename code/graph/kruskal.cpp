abeec536844d3e17a640286f49013cded98abbb1
ogjoaoc <joaocarlosgoncalvesfilho@gmail.com>
2024-09-24 20:31:51 -0300

    kruskal

---
+++ code/graph/kruskal.cpp
@@ -0,0 +1,50 @@
+// Ordena as arestas por peso,  insere se ja nao estiver no mesmo componente
+// O(E log E)
+struct DSU {
+    vector<int> par, rank, sz;
+    int c;
+    DSU(int n) : par(n + 1), rank(n + 1, 0), sz(n + 1, 1), c(n) {
+        for (int i = 1; i <= n; ++i) par[i] = i;
+    }
+    int find(int i) {
+        return (par[i] == i ? i : (par[i] = find(par[i])));
+    }
+    bool same(int i, int j) {
+        return find(i) == find(j);
+    }
+    int get_size(int i) {
+        return sz[find(i)];
+    }
+    int count() {
+        return c;  // quantos componentes conexos
+    }
+    int merge(int i, int j) {
+        if ((i = find(i)) == (j = find(j))) return -1;
+        else --c;
+        if (rank[i] > rank[j]) swap(i, j);
+        par[i] = j;
+        sz[j] += sz[i];
+        if (rank[i] == rank[j]) rank[j]++;
+        return j;
+    }
+};
+
+struct Edge {
+    int u, v, w;
+    bool operator <(Edge const & other) {
+        return weight <other.weight;
+    }
+}
+
+vector<Edge> kruskal(int n, vector<Edge> edges) {
+    vector<Edge> mst;
+    DSU dsu = DSU(n + 1);
+    sort(edges.begin(), edges.end());
+    for (Edge e : edges) {
+        if (dsu.find(e.u) != dsu.find(e.v)) {
+            mst.push_back(e);
+            dsu.join(e.u, e.v);
+        }
+    }
+    return mst;
+}
