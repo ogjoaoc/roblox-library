fc139af46a9344ffff09c450aa61e845fb331301
ogjoaoc <joaocarlosgoncalvesfilho@gmail.com>
2024-09-24 18:19:57 -0300

    testing

--- code/dp/knapsack.cpp
+++ code/dp/knapsack.cpp
@@ -2,15 +2,15 @@
 // O(N * W)
 
 for(int j = 0; j < MAXN; j++) {
-        dp[0][j] = 0;
-    }
-    for(int i = 1; i <= N; i++) {
-        for(int j = 0; j <= W; j++) {
-            if(items[i].first > j) {
-                dp[i][j] = dp[i-1][j];
-            } 
-            else {
-                dp[i][j] = max(dp[i-1][j], dp[i-1][j-items[i].first] + items[i].second);
-            }
+    dp[0][j] = 0;
+}
+for(int i = 1; i <= N; i++) {
+    for(int j = 0; j <= W; j++) {
+        if(items[i].first > j) {
+            dp[i][j] = dp[i-1][j];
+        } 
+        else {
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-items[i].first] + items[i].second);
         }
     }
+}
