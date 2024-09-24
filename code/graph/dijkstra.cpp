10e28e7629871b3452e41323605059317e4e1276
ogjoaoc <joaocarlosgoncalvesfilho@gmail.com>
2024-09-24 17:35:23 -0300

    dijkstra

---
+++ code/graph/dijkstra.cpp
@@ -0,0 +1,22 @@
+vector<pii> adj[MAXN];
+ 
+vector<int> dijkstra(int S) {
+    vector<bool> vis(MAXN, 0);
+    vector<ll> dist(MAXN, LLONG_MAX);
+    dist[S] = 0;
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
+    pq.push({0, S});
+    while(pq.size()) {
+        ll v = pq.top().second;
+        pq.pop();
+        if(vis[v]) continue;
+        vis[v] = 1;
+        for(auto &[peso, vizinho] : adj[v]) {
+            if(dist[vizinho] > dist[v] + peso) {
+                dist[vizinho] = dist[v] + peso;
+                pq.push({dist[vizinho], vizinho});
+            }
+        }
+    }
+    return dist;
+}
