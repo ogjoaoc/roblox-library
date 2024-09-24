fbc595be374bebf72ac8eb528c646351da53bd39
Arthur Bispo <arthur.bispo4@gmail.com>
2024-09-24 19:09:14 -0300

    Math update

--- code/math/exgcd.cpp
+++ code/math/exgcd.cpp
@@ -1,6 +1,6 @@
-// O retorno da função é {n, m, g}
+// O retorno da funcao eh {n, m, g}
 // e significa que gcd(a, b) = g e
-// n e m são inteiros tais que an + bm = g
+// n e m sao inteiros tais que an + bm = g
 array<ll, 3> exgcd(int a, int b) {
     if(b == 0) return {1, 0, a};
     auto [m, n, g] = exgcd(b, a % b);
