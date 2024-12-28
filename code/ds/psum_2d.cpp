098e84819bebd056c9db0079f808e3b638369cfe
Arthur Bispo <64386652+arthurxsz@users.noreply.github.com>
2024-12-28 12:23:56 -0300

    Update psum_2d.cpp

--- code/ds/psum_2d.cpp
+++ code/ds/psum_2d.cpp
@@ -8,8 +8,8 @@ for (int i=1; i<n+1; i++){
 }
 
 int retangulo(int x1, int y1, int x2, int y2){
-    z = min(x2, n), w = min(y2, m);
-    x = max(0LL, x1-1), y = max(0LL, y1-1);
+    x2 = min(x2, n), y2 = min(y2, m);
+    x1 = max(0LL, x1-1), y1 = max(0LL, y1-1);
 
     return psum[x2][y2]-psum[x1][y2]-psum[x2][y1]+psum[x1][y1];
 }
