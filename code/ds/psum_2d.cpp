9bd8b1246f2bde58359083830759fb78caafa785
Arthur Bispo <64386652+arthurxsz@users.noreply.github.com>
2024-12-30 12:51:19 -0300

    Update psum_2d.cpp

--- code/ds/psum_2d.cpp
+++ code/ds/psum_2d.cpp
@@ -10,8 +10,8 @@ for (int i=1; i<n+1; i++){
 
 // y1 eh variavel reservada
 int retangulo(int x1, int yy1, int x2, int yy2){
-    x2 = min(x2, n), y2 = min(y2, m);
-    x1 = max(0LL, x1-1), y1 = max(0LL, y1-1);
+    x2 = min(x2, n), yy2 = min(yy2, m);
+    x1 = max(0LL, x1-1), yy1 = max(0LL, yy1-1);
 
-    return psum[x2][y2]-psum[x1][y2]-psum[x2][y1]+psum[x1][y1];
+    return psum[x2][yy2]-psum[x1][yy2]-psum[x2][yy1]+psum[x1][yy1];
 }
