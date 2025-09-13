27c3fd46398257fc3020780141d047f6b1e3653c
ogjoaoc <joaocarlosgoncalvesfilho@gmail.com>
2025-09-13 00:59:34 -0300

    pronto

---
+++ code/search and sort/pilha_monotonic.cpp
@@ -0,0 +1,37 @@
+vector<int> find_esq(vector<int> &v, bool maior) {
+    int n = v.size();
+    vector<int> result(n);
+    stack<int> s; 
+
+    for (int i = 0; i < n; i++) {
+        while (!s.empty() && (maior ? v[s.top()] <= v[i] : v[s.top()] >= v[i])) {
+            s.pop();
+        }
+        if (s.empty()) {
+            result[i] = -1; 
+        } else {
+            result[i] = v[s.top()];
+        }
+        s.push(i);
+    }
+    return result;
+}
+
+// maior = true -> encontra o primeiro maior à direita
+vector<int> find_dir(vector<int> &v, bool maior) {
+    int n = v.size();
+    vector<int> result(n);
+    stack<int> s; 
+    for (int i = n - 1; i >= 0; i--) {
+        while (!s.empty() && (maior ? v[s.top()] <= v[i] : v[s.top()] >= v[i])) {
+            s.pop();
+        }
+        if (s.empty()) {
+            result[i] = -1;
+        } else {
+            result[i] = v[s.top()];
+        }
+        s.push(i);
+    }
+    return result;
+}
