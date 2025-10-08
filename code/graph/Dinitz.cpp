715211508c7cd42e2e09941c47765d971698b852
Arthur Bispo <64386652+arthurxsz@users.noreply.github.com>
2025-10-08 16:11:32 -0300

    Refactor Dinitz algorithm variable names for clarity

--- code/graph/Dinitz.cpp
+++ code/graph/Dinitz.cpp
@@ -1,9 +1,9 @@
 // Complexidade: O(V^2E)
 
 struct FlowEdge {
-    int v, u;
+    int from, to;
     long long cap, flow = 0;
-    FlowEdge(int v, int u, long long cap) : v(v), u(u), cap(cap) {}
+    FlowEdge(int from, int to, long long cap) : from(from), to(to), cap(cap) {}
 };
 
 struct Dinic {
@@ -21,41 +21,41 @@ struct Dinic {
         ptr.resize(n);
     }
 
-    void add_edge(int v, int u, long long cap) {
-        edges.emplace_back(v, u, cap);
-        edges.emplace_back(u, v, 0);
-        adj[v].push_back(m);
-        adj[u].push_back(m + 1);
+    void add_edge(int from, int to, long long cap) {
+        edges.emplace_back(from, to, cap);
+        edges.emplace_back(to, from, 0);
+        adj[from].push_back(m);
+        adj[to].push_back(m + 1);
         m += 2;
     }
 
     bool bfs() {
         while (!q.empty()) {
-            int v = q.front();
+            int from = q.front();
             q.pop();
-            for (int id : adj[v]) {
+            for (int id : adj[from]) {
                 if (edges[id].cap == edges[id].flow)
                     continue;
-                if (level[edges[id].u] != -1)
+                if (level[edges[id].to] != -1)
                     continue;
-                level[edges[id].u] = level[v] + 1;
-                q.push(edges[id].u);
+                level[edges[id].to] = level[from] + 1;
+                q.push(edges[id].to);
             }
         }
         return level[t] != -1;
     }
 
-    long long dfs(int v, long long pushed) {
+    long long dfs(int from, long long pushed) {
         if (pushed == 0)
             return 0;
-        if (v == t)
+        if (from == t)
             return pushed;
-        for (int& cid = ptr[v]; cid < (int)adj[v].size(); cid++) {
-            int id = adj[v][cid];
-            int u = edges[id].u;
-            if (level[v] + 1 != level[u])
+        for (int& cid = ptr[from]; cid < (int)adj[from].size(); cid++) {
+            int id = adj[from][cid];
+            int to = edges[id].to;
+            if (level[from] + 1 != level[to])
                 continue;
-            long long tr = dfs(u, min(pushed, edges[id].cap - edges[id].flow));
+            long long tr = dfs(to, min(pushed, edges[id].cap - edges[id].flow));
             if (tr == 0)
                 continue;
             edges[id].flow += tr;
