322656a2a47c7ef85731f10ba57ce7a83f009af6
João Carlos <110072806+ogjoaoc@users.noreply.github.com>
2025-12-30 14:14:51 -0300

    refactor kmp

--- code/string/kmp.cpp
+++ code/string/kmp.cpp
@@ -1,12 +1,10 @@
-vector<int> kmp(string s) {
-    int n = (int)s.length();
-    vector<int> p(n+1);
-    p[0] = -1;
-    for (int i = 1; i < n; i++) {
-        int j = p[i-1];
-        while (j >= 0 && s[j] != s[i-1])
-            j = p[j-1];
-        p[i] = j+1;
+vector<int> kmp(string &s) {
+    int m = s.size();
+    vector<int> lsp(m, 0);
+    for (int i = 1, j = 0; i < m; i++) { 
+        while (j > 0 && s[j] != s[i]) j = lsp[j - 1];
+        if (s[i] == s[j]) j++;
+        lsp[i] = j;
     }
-    return p;
+    return lsp;
 }
