40dcc0f4c5f06d54a4c4f42a46a0db482da4045e
João Carlos <110072806+ogjoaoc@users.noreply.github.com>
2026-02-13 14:51:44 -0300

    small fix

--- code/math/combinatorics.cpp
+++ code/math/combinatorics.cpp
@@ -1,7 +1,7 @@
-const int MAXN_FATORIAL = 200005; 
+const int N = 200005; 
 const int MOD = 1e9 + 7;
 // DEFINE INT LONG LONG PLMDS
-int fat[MAXN_FATORIAL], fati[MAXN_FATORIAL];
+int fat[N], fati[N];
 
 // (a^b) % m em O(log b)
 // coloque o fexp
@@ -11,9 +11,9 @@ int inv(int n) { return fexp(n, MOD - 2); }
 void precalc() {
     fat[0] = 1;
     fati[0] = 1;
-    for (int i = 1; i < MAXN_FATORIAL; i++) fat[i] = (fat[i - 1] * i) % MOD;
-    fati[MAXN_FATORIAL - 1] = inv(fat[MAXN_FATORIAL - 1]);
-    for (int i = MAXN_FATORIAL - 2; i >= 0; i--) fati[i] = (fati[i + 1] * (i + 1)) % MOD;
+    for (int i = 1; i < N; i++) fat[i] = (fat[i - 1] * i) % MOD;
+    fati[N - 1] = inv(fat[N - 1]);
+    for (int i = N - 2; i >= 0; i--) fati[i] = (fati[i + 1] * (i + 1)) % MOD;
 }
 
 int choose(int n, int k) {
@@ -29,7 +29,8 @@ int perm(int n, int k) {
 
 // C_n = (1 / (n+1)) * C(2n, n)
 int catalan(int n) {
-    if (n < 0 || 2 * n >= MAXN_FATORIAL) return 0;
+    if (n < 0 || 2 * n >= N) return 0;
     int c2n_n = choose(2 * n, n);
     return (c2n_n * inv(n + 1)) % MOD;
+
 }
